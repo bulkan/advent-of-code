8b9f66aee4a5dd1ea75546713ffc058fe9e59710
Bulkan Evcimen <bulkan@gmail.com>
2023-12-26 16:14:04 +1100

    Get string digits into matches

--- 2023/day-1/src/bin/part2.rs
+++ 2023/day-1/src/bin/part2.rs
@@ -1,23 +1,23 @@
 use regex::Regex;
-use std::{collections::HashMap, fs};
+use std::fs;
 
 fn main() {
-    let numbers_map = HashMap::from([
-        ("zero", 0),
-        ("one", 1),
-        ("two", 2),
-        ("three", 3),
-        ("four", 4),
-        ("five", 5),
-        ("six", 6),
-        ("seven", 7),
-        ("eight", 8),
-        ("nine", 9),
-    ]);
+    // let numbers_map = HashMap::from([
+    //     ("one", 1),
+    //     ("two", 2),
+    //     ("three", 3),
+    //     ("four", 4),
+    //     ("five", 5),
+    //     ("six", 6),
+    //     ("seven", 7),
+    //     ("eight", 8),
+    //     ("nine", 9),
+    // ]);
 
     let content = fs::read_to_string("calibrationValues.txt").expect("file no found");
 
-    let re = Regex::new(r"\d").expect("failed to create regex");
+    let re = Regex::new(r"(\d+)|(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)+")
+        .expect("failed to create regex");
 
     let sum = content.split('\n').fold(0, |acc, line| {
         let digit_matches = re
@@ -25,19 +25,21 @@ fn main() {
             .map(|mat| mat.get(0).unwrap().as_str())
             .collect::<Vec<_>>();
 
-        if digit_matches.is_empty() {
-            return acc;
-        }
+        println!("{:?}", digit_matches);
 
-        let digit = format!(
-            "{}{}",
-            digit_matches[0],
-            digit_matches[digit_matches.len() - 1]
-        );
+        // if digit_matches.is_empty() {
+        //     return acc;
+        // }
 
-        if let Ok(result) = digit.parse::<u32>() {
-            return acc + result;
-        }
+        // let digit = format!(
+        //     "{}{}",
+        //     digit_matches[0],
+        //     digit_matches[digit_matches.len() - 1]
+        // );
+        //
+        // if let Ok(result) = digit.parse::<u32>() {
+        //     return acc + result;
+        // }
 
         acc
     });
