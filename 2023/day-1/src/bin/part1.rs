8e1b8671e6109503b5046a10ce0f6d99b388d9a2
Bulkan Evcimen <bulkan@gmail.com>
2023-12-29 20:20:07 +1100

    Refactor day-1/part1 to avoid using regex

--- 2023/day-1/src/bin/part1.rs
+++ 2023/day-1/src/bin/part1.rs
@@ -1,33 +1,23 @@
-use regex::Regex;
 use std::fs;
 
 fn main() {
     let content = fs::read_to_string("calibrationValues.txt").expect("file no found");
 
-    let re = Regex::new(r"\d").expect("failed to create regex");
+    let sum = content
+        .lines()
+        .map(|line| {
+            let mut it = line.chars().filter_map(|c| c.to_digit(10));
 
-    let sum = content.split('\n').fold(0, |acc, line| {
-        let digit_matches = re
-            .captures_iter(line)
-            .map(|mat| mat.get(0).unwrap().as_str())
-            .collect::<Vec<_>>();
+            let first = it.next().expect("should be a number");
 
-        if digit_matches.is_empty() {
-            return acc;
-        }
-
-        let digit = format!(
-            "{}{}",
-            digit_matches[0],
-            digit_matches[digit_matches.len() - 1]
-        );
-
-        if let Ok(result) = digit.parse::<u32>() {
-            return acc + result;
-        }
-
-        acc
-    });
+            match it.last() {
+                Some(num) => format!("{first}{num}"),
+                None => format!("{first}{first}"),
+            }
+            .parse::<u32>()
+            .expect("should be a number")
+        })
+        .sum::<u32>();
 
     println!("{sum}");
 }
