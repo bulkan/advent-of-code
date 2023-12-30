351fb22faa9d917044c65b322265dba1ac738845
Bulkan Evcimen <bulkan@gmail.com>
2023-12-31 01:39:03 +1100

    Initial thinking around day-3

--- 2023/day-3/src/bin/part1.rs
+++ 2023/day-3/src/bin/part1.rs
@@ -1,3 +1,49 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let schematic = fs::read_to_string("input.txt").expect("file should exist");
+
+    let res = gear_ratios(schematic.trim());
+
+    dbg!(res);
+}
+
+// current line will need to peek at second line
+//    if there is second line then while iterating over first line
+//        skip over periods, when a digit is found,
+//            keep track of start pos and end pos, digit as string
+//
+fn gear_ratios(schematic: &str) -> u32 {
+    let lines_of_chars = schematic
+        .lines()
+        .map(|l| l.chars().collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+
+    dbg!(lines_of_chars);
+
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::gear_ratios;
+
+    #[test]
+    fn it_works_as_expected() {
+        let schematic = "
+467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598.."
+            .trim();
+
+        let result = gear_ratios(schematic);
+        assert_eq!(result, 4361);
+    }
 }
