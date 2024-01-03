c0ddd4f8e6c7fb8608b96e202e8f41eee54b37fa
Bulkan Evcimen <bulkan@gmail.com>
2024-01-03 11:28:20 +1100

    :test_tube: tests

--- 2023/day-06/src/bin/part1.rs
+++ 2023/day-06/src/bin/part1.rs
@@ -1,37 +1,68 @@
-use nom::{
-    bytes::complete::tag,
-    character::complete::{digit1, space0, space1},
-    combinator::map_res,
-    multi::separated_list1,
-    sequence::separated_pair,
-    IResult,
-};
+use indoc::indoc;
 use std::{
     collections::{BTreeMap, HashSet},
     u32,
 };
 
 fn main() {
-    let scratchcards = include_str!("./input.txt");
-
-    let points = process_scratchcards(scratchcards);
-
-    println!("{points}");
+    let races = indoc! {"
+        Time:        60     80     86     76
+        Distance:   601   1163   1559   1300
+    "};
 }
 
+#[derive(Debug)]
+struct Race {
+    time: u32,
+    distance: u32,
+}
+
+fn parse_races(input: &str) -> Vec<Race> {
+    todo!()
+}
+
+fn possible_races(duration: u32) -> HashSet<u32> {
+    (0..duration + 1)
+        .map(|hold_time| {
+            let speed = hold_time;
+
+            let remaining_duration = duration - speed;
+
+            remaining_duration * speed
+        })
+        .collect::<HashSet<u32>>()
+}
+
+fn process(input: &str) -> u32 {
+    let duration = 7;
+
+    dbg!(possible_races(duration));
+    0
+}
 
 #[cfg(test)]
 mod tests {
-    use indoc::indoc;
     use std::collections::HashSet;
+
+    use crate::{possible_races, process};
+    use indoc::indoc;
+
+    #[test]
+    fn possible_races_returns_correct_values() {
+        let races = possible_races(7);
+
+        assert_eq!(races, HashSet::from([6, 10, 12, 0]));
+    }
 
     #[test]
     fn it_works_as_expected() {
         let input = indoc! {"
-        };
+            Time:      7  15   30
+            Distance:  9  40  200
+        "};
 
-        let res = process(input);
+        let margin_of_error = process(input);
 
-        assert_eq!(res, 30);
+        assert_eq!(margin_of_error, 288);
     }
 }
