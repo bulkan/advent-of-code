b55d3a729f8fd1b8e283183e9ac17340c362d810
Bulkan Evcimen <bulkan@gmail.com>
2024-01-06 14:13:17 +1100

    Implement ranking of hands

--- 2023/day-07/src/hand.rs
+++ 2023/day-07/src/hand.rs
@@ -1,13 +1,57 @@
 use std::collections::{BTreeMap, HashMap};
 
 #[derive(Debug, PartialEq)]
+enum PokerHand {
+    HighCard,
+    OnePair,
+    TwoPair,
+    FullHouse,
+    ThreeOfAKind,
+    FourOfAKind,
+    FiveOfAKind,
+    Unknown,
+}
+
+#[derive(Debug, PartialEq)]
 pub struct Hand<'a> {
     cards: Vec<&'a str>,
-    frequency: BTreeMap<&'a str, u32>,
     bet: u32,
+    rank: PokerHand,
 }
 
 impl<'a> Hand<'a> {
+    fn get_rank(frequency: BTreeMap<&str, u32>) -> PokerHand {
+        match frequency.len() {
+            1 => PokerHand::FiveOfAKind,
+            2 => {
+                let mut values = frequency.values();
+                if values.all(|v| *v == 3 || *v == 2) {
+                    PokerHand::FullHouse
+                } else if values.all(|v| *v == 4 || *v == 1) {
+                    PokerHand::FourOfAKind
+                } else {
+                    PokerHand::Unknown
+                }
+            }
+            3 => {
+                let mut values = frequency.values();
+
+                if values.any(|v| *v == 3) {
+                    PokerHand::ThreeOfAKind
+                } else if values.all(|v| *v == 2 || *v == 1) {
+                    PokerHand::TwoPair
+                } else {
+                    PokerHand::Unknown
+                }
+            }
+            4 => PokerHand::OnePair,
+            5 => PokerHand::HighCard,
+            _ => PokerHand::Unknown,
+        }
+
+        // frequency.len() == self.frequency.values().all(|v| *v == 3 || *v == 2)
+    }
+
     pub fn new(cards: Vec<&'a str>, bet: u32) -> Hand {
         let frequency: BTreeMap<&str, u32> = cards.iter().fold(BTreeMap::new(), |mut acc, card| {
             acc.entry(card).and_modify(|card| *card += 1).or_insert(1);
@@ -18,7 +62,7 @@ impl<'a> Hand<'a> {
         Hand {
             cards,
             bet,
-            frequency,
+            rank: Hand::get_rank(frequency),
         }
     }
 
@@ -35,17 +79,30 @@ impl<'a> Hand<'a> {
             })
             .sum::<u32>()
     }
-
-    pub fn is_full_house(&self) -> bool {
-        self.frequency.len() == 2 && self.frequency.values().any(|frequency| *frequency == 3)
-    }
 }
 
 #[cfg(test)]
 mod tests {
     use std::collections::BTreeMap;
 
-    use crate::hand::Hand;
+    use crate::hand::{Hand, PokerHand};
+
+    #[test]
+    fn hand_rank_is_calculated_correctly() {
+        let high_card = Hand::new(vec!["3", "2", "T", "A", "K"], 1);
+        let pair = Hand::new(vec!["3", "2", "T", "3", "K"], 1);
+        let two_pair = Hand::new(vec!["3", "2", "K", "3", "K"], 1);
+        let three_of_a_kind = Hand::new(vec!["T", "5", "5", "J", "5"], 1);
+        let five_of_a_kind = Hand::new(vec!["5", "5", "5", "5", "5"], 1);
+        let four_of_a_kind = Hand::new(vec!["5", "5", "5", "J", "5"], 1);
+
+        assert_eq!(high_card.rank, PokerHand::HighCard);
+        assert_eq!(five_of_a_kind.rank, PokerHand::FiveOfAKind);
+        assert_eq!(four_of_a_kind.rank, PokerHand::FourOfAKind);
+        assert_eq!(pair.rank, PokerHand::OnePair);
+        assert_eq!(two_pair.rank, PokerHand::TwoPair);
+        assert_eq!(three_of_a_kind.rank, PokerHand::ThreeOfAKind);
+    }
 
     #[test]
     fn hand_strength_works() {
@@ -61,16 +118,16 @@ mod tests {
     #[test]
     fn hand_frequency_is_correct() {
         let full_house = Hand::new(vec!["K", "K", "K", "A", "A"], 0);
-        assert_eq!(full_house.frequency, BTreeMap::from([("K", 3), ("A", 2)]));
+        // assert_eq!(full_house.frequency, BTreeMap::from([("K", 3), ("A", 2)]));
 
-        assert!(full_house.is_full_house());
+        // assert!(!full_house.is_full_house());
 
         let pair = Hand::new(vec!["K", "K", "2", "A", "3"], 0);
-        assert_eq!(
-            pair.frequency,
-            BTreeMap::from([("K", 2), ("A", 1), ("2", 1), ("3", 1)])
-        );
+        // assert_eq!(
+        //     pair.frequency,
+        //     BTreeMap::from([("K", 2), ("A", 1), ("2", 1), ("3", 1)])
+        // );
 
-        assert!(!pair.is_full_house());
+        // assert!(!pair.is_full_house());
     }
 }
