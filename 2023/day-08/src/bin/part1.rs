242ac1a68a7444721d982f01e160ec292871febb
Bulkan Evcimen <bulkan@gmail.com>
2024-01-07 02:08:53 +1100

    Begin writing nom parser for day 08

--- 2023/day-08/src/bin/part1.rs
+++ 2023/day-08/src/bin/part1.rs
@@ -1,4 +1,12 @@
+use std::{char, os::unix::fs::DirEntryExt};
+
 use indoc::indoc;
+use nom::{
+    bytes::complete::take_while,
+    character::{complete::newline, is_newline},
+    IResult,
+};
+use nom_supreme::error::ErrorTree;
 
 fn main() {
     todo!()
@@ -8,10 +16,32 @@ fn process(input: &str) -> u32 {
     0
 }
 
+fn parse_instructions(input: &str) -> IResult<&str, Vec<char>, ErrorTree<&str>> {
+    let (input, directions) = take_while(|c| !is_newline(c as u8))(input)?;
+
+    let directions = directions.chars().collect::<Vec<_>>();
+
+    Ok((input, directions))
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::process;
+    use crate::{parse_instructions, process};
     use indoc::indoc;
+
+    #[test]
+    fn parse_instructions_works() {
+        let input = indoc! {"
+            LLR
+
+            AAA = (BBB, BBB)
+        "};
+
+        assert_eq!(
+            parse_instructions(input).unwrap(),
+            ("\n\nAAA = (BBB, BBB)\n", vec!['L', 'L', 'R']),
+        );
+    }
 
     #[test]
     fn it_works_as_expected() {
