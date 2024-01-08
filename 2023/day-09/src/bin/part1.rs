2a0dd784223bf7a95339829dfb095f5777d23f44
Bulkan Evcimen <bulkan@mini.fritz.box>
2024-01-08 18:40:58 +1100

    Trying to think about how to generate the value differences for next line

--- 2023/day-09/src/bin/part1.rs
+++ 2023/day-09/src/bin/part1.rs
@@ -10,11 +10,30 @@ fn process(input: &str) -> u32 {
     todo!()
 }
 
+fn generate_difference(input: Vec<i32>) -> Vec<Vec<i32>> {
+    vec![]
+}
+
 #[cfg(test)]
 mod tests {
     use indoc::indoc;
 
-    use crate::process;
+    use crate::{generate_difference, process};
+
+    #[test]
+    fn generate_difference_calcuation_works() {
+        let input = vec![10, 13, 16, 21, 30, 45];
+
+        let expected = vec![
+            vec![10, 13, 16, 21, 30, 45],
+            vec![3, 3, 5, 9, 15, 23],
+            vec![0, 2, 4, 6, 8],
+            vec![2, 2, 2, 2],
+            vec![0, 0, 0],
+        ];
+
+        assert_eq!(generate_difference(input), expected);
+    }
 
     #[test]
     fn example_should_work() {
