8a3c264b7a334df643c78d4cda09a1d0f575968b
Bulkan Evcimen <bulkan@gmail.com>
2024-12-11 22:19:28 +1100

    [day-10] indexing into Vec<Vec<_>> is hard

--- 2024/day-10/src/bin/part1.rs
+++ 2024/day-10/src/bin/part1.rs
@@ -1,75 +1,62 @@
-use std::usize;
+use glam::IVec2;
+use std::collections::{HashMap, HashSet};
 
-use itertools::Itertools;
+const DIRECTIONS: [IVec2; 4] = [IVec2::X, IVec2::NEG_X, IVec2::Y, IVec2::NEG_Y];
 
-fn process(input: &str) -> u32 {
-    let directions: Vec<(i32, i32)> = vec![
-        (0, -1), // Up
-        (1, 0),  // Right
-        (0, 1),  // Down
-        (-1, 0), // Left
-    ];
-
+fn process(input: &str) -> usize {
     let grid = input
         .lines()
-        .map(|line| {
+        .enumerate()
+        .flat_map(|(y, line)| {
             line.chars()
-                .map(|c| c.to_digit(10).expect("should be able to parse"))
+                .enumerate()
+                .map(|(x, c)| {
+                    let pos = IVec2::new(x as i32, y as i32);
+                    let v = c.to_digit(10).expect("should be able to parse");
+
+                    (pos, v)
+                })
                 .collect::<Vec<_>>()
         })
-        .collect::<Vec<_>>();
+        .collect::<HashMap<IVec2, u32>>();
 
-    let zeros = grid
-        .iter()
-        .enumerate()
-        .map(|(y, row)| {
-            row.iter()
-                .enumerate()
-                .filter_map(
-                    move |(x, height)| {
-                        if *height == 0 {
-                            Some((x, y))
-                        } else {
-                            None
-                        }
-                    },
-                )
-                .collect_vec()
-        })
-        .flatten()
-        .collect_vec();
+    grid.iter()
+        .filter(|(_, height)| **height == 0)
+        .map(|(position, _)| {
+            let mut visited: HashSet<IVec2> = HashSet::from([]);
+            let mut new_locations: HashSet<IVec2> = HashSet::from([*position]);
+            while !new_locations.is_empty() {
+                let newer_locations = new_locations
+                    .iter()
+                    .flat_map(|starting_location| {
+                        DIRECTIONS
+                            .iter()
+                            .map(move |dir| (dir + starting_location, starting_location))
+                    })
+                    .filter(|(new_location, starting_location)| {
+                        !new_locations.contains(new_location)
+                            && grid.get(new_location).is_some_and(|h| {
+                                let current_height = grid.get(starting_location).unwrap();
 
-    let grid_width = grid.first().expect("should exist").len() as i32;
-    let grid_height = grid.len() as i32;
+                                *h == current_height + 1
+                            })
+                    })
+                    .map(|(new, _)| new)
+                    .collect::<HashSet<IVec2>>();
 
-    zeros
-        .iter()
-        .map(|(x, y)| {
-            let mut current = 0;
-
-            let next_direction = directions.iter().find(|(dx, dy)| {
-                let next_x = *x as i32 + dx;
-                let next_y = *y as i32 + dy;
-
-                if next_x < 0 || next_x > grid_width || next_y < 0 || next_y > grid_height {
-                    return false;
+                for loc in newer_locations.iter() {
+                    visited.insert(*loc);
                 }
 
-                let next_val = grid
-                    .get(next_y as usize)
-                    .and_then(|row| row.get(next_x as usize))
-                    .copied();
+                new_locations = newer_locations;
+            }
 
-                if Some(current + 1) == next_val {
-                    dbg!(next_val, current);
-                }
-
-                false
-            });
+            visited
+                .iter()
+                .filter(|pos| grid.get(pos).unwrap() == &9)
+                .count()
         })
-        .collect_vec();
-
-    0
+        .sum()
 }
 
 fn main() {
@@ -84,11 +71,14 @@ mod tests {
 
     #[test]
     fn test_process() {
-        let input = "0123
-1234
-8765
-9876
-";
+        let input = "89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732";
         assert_eq!(36, process(input));
     }
 }
